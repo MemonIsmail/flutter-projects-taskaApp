887f818a71440ce7088acbe89963b310be3fca55
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-01 08:25:32 +0500

    Enhance CustomTextField with focus and add CustomElevatedButton
    Added focus handling and new button widget.

--- lib/screens/widgets.dart
+++ lib/screens/widgets.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:taska_app/controllers/text_field_controller.dart';
 import 'package:taska_app/screens/screens_colors.dart';
 
 class CustomHeadingText extends StatelessWidget {
@@ -22,6 +24,8 @@ class CustomHeadingText extends StatelessWidget {
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController? textEditingController;
+  final RxBool isFocused;
+  final FocusNode focusNode;
   final bool obscureText;
   final IconData? suffIcon;
   final IconData? prefIcon;
@@ -36,7 +40,9 @@ class CustomTextField extends StatelessWidget {
   final double borderWidth;
   const CustomTextField({
     super.key,
-    this.textEditingController,
+    required this.isFocused,
+    required this.textEditingController,
+    required this.focusNode,
     this.onChanged,
     required this.hintText,
     this.obscureText=false,
@@ -53,46 +59,71 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: textEditingController,
-      obscureText: obscureText,
-      decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: TextStyle(
-            color: kGreyColor,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(borderRadius),
-          ),
-          suffixIcon: IconButton(
-              icon: Icon(suffIcon),
-              onPressed:onPressed
-          ),
-          suffixIconColor: kGreyColor,
-          prefixIcon: prefIcon != null ? Icon(prefIcon) : null,
-          prefixIconConstraints: prefIcon == null
-              ? const BoxConstraints()
-              : null,
-          prefixIconColor: kGreyColor,
-          filled: fill,
-          fillColor: filColor,
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(borderRadius),
-              borderSide: BorderSide(
-                color: borderColor,
-                width: borderWidth,
-              )
-          ),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(borderRadius),
-              borderSide: BorderSide(
-                color: borderColor,
-                width: borderWidth,
-              )
-          )
-      ),
-      onChanged: onChanged,
-      onTap: onTap,
+    return Obx(() {
+      bool focused = isFocused.value;
+      return TextField(
+        controller: textEditingController,
+        focusNode: focusNode,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: kGreyColor,
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(borderRadius),
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                )
+            ),
+            suffixIcon: IconButton(
+                icon: Icon(suffIcon),
+                onPressed:onPressed
+            ),
+            suffixIconColor: kGreyColor,
+            prefixIcon: Icon(
+                prefIcon,
+                color: focused
+                  ? Colors.black
+                  : kGreyColor,
+            ),
+            filled: true,
+            fillColor: focused
+              ? kWhiteColor
+              : kLightGreyColor,
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(borderRadius),
+                borderSide: BorderSide(
+                  color: kIndigoAccent,
+                  width: 1.5,
+                )
+            )
+        ),
+        onChanged: onChanged,
+        onTap: onTap,
+      );
+    }
     );
   }
 }
+
+class CustomElevatedButton extends StatelessWidget {
+  final String text;
+  final Color buttonColor;
+  final Color textColor;
+  const CustomElevatedButton({super.key, required this.text, required this.buttonColor, required this.textColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: (){},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor,
+        foregroundColor: textColor,
+        // elevation: 0.0
+      ),
+      child: Text(text),
+    );
+  }
+}
+
